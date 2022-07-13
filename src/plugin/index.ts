91958e8689cc59e23a62bca61775dfe5efe9732f
TheNonStopGamer <rickopdekop@gmail.com>
2022-07-14 00:33:38 +0200

    Added DaikErrorLogPlugin

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -1,4 +1,6 @@
 export * from './plugins/DaikBlankPlugin.js';
+export * from './plugins/DaikErrorLogPlugin.js';
+export * from './plugins/DaikPermissionPlugin.js';
 
 export * from './DaikPlugin.js';
 export * from './DaikPluginGroup.js';
