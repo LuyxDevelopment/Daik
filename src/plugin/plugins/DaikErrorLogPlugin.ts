782f656579980459e5ae02f88a63b89a0a34f99d
TheNonStopGamer <rickopdekop@gmail.com>
2022-07-14 21:54:50 +0200

    Fixed module registration on error log plugin

--- src/plugin/plugins/DaikErrorLogPlugin.ts
+++ src/plugin/plugins/DaikErrorLogPlugin.ts
@@ -3,7 +3,7 @@ import { DaikPluginModule } from '../../typings/daik_plugin.js';
 import { DaikPlugin } from '../DaikPlugin.js';
 
 export class DaikErrorLogPlugin<RA extends DaikCommandRunArgs, R extends DaikCommandResult, P extends DaikCommandProps> extends DaikPlugin<RA, R, P> {
-	protected registrableModules: DaikPluginModule[] = ['preCommand'];
+	protected registrableModules: DaikPluginModule[] = ['onError'];
 
 	public onError(error: unknown): void {
 		console.error(error);
